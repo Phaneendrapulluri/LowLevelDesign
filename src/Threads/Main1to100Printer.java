6113d44be4f5984846c3fb2cc15aa2e68960e402
Phaneendra Pulluri <phaneepulluri@gmail.com>
2024-01-22 23:27:25 +0530

    Executors

--- src/Threads/Main1to100Printer.java
+++ src/Threads/Main1to100Printer.java
@@ -1,11 +1,23 @@
 package Threads;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main1to100Printer {
     public static void main(String[] args) {
+        //Executor service will provide thread pools
+     //   ExecutorService es1 = Executors.newFixedThreadPool(10);
+        ExecutorService es2 = Executors.newCachedThreadPool();
         for(int i=1;i<=100;i++){
             Printer1to100 p=new Printer1to100(i);
-            Thread t=new Thread(p);
-            t.start();
+//            Thread t=new Thread(p);
+//            t.start();
+            es2.execute(p);
+
         }
+        es2.shutdown();
+
+
+
     }
 }
